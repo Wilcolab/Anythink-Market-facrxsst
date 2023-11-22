c90bc700f47adfd56e2245e947185abfa1166353
hiteshchouhan22 <24716760+hiteshchouhan22@users.noreply.github.com>
2023-11-22 12:11:28 +0000

    Seeding Script added

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,98 +1,80 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
-const moongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 require('../models/User');
 require('../models/Item');
 require('../models/Comment');
 
-var Item = moongoose.model('Item');
+const Item = mongoose.model('Item');
+const Comment = mongoose.model('Comment');
+const User = mongoose.model('User');
 
-var Comment = moongoose.model('Comment');
-
-var User = moongoose.model('User');
-
-
-//connect to Mongo DB
-
-if(process.MONGODB_URI){
-    moongoose.connect(process.env.MONGODB_URI)
-}
-else{
-    console.warn(`Missing MONGODB_URI in the env`)
+// Connect to MongoDB
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    console.warn('Missing MONGODB_URI in the environment');
 }
 
-let userId;
+async function seedDatabase() {
+    const users = Array.from(Array(100)).map((_item, i) => ({
+        username: `fakeuser${i}`,
+        email: `fakeuser${i}@anythinks.com`,
+        bio: 'test bio',
+        image: 'https://picsum.photos/200',
+        role: 'user',
+        favorites: [],
+        following: [],
+    }));
 
-let itemId;
+    for (let user of users) {
+        const existingUser = await User.findOne({ username: user.username, email: user.email });
 
-
-async function seedDatabase(){
-
- const users = Array.from(Array(100)).map((_item,i)=>({
-    username: `fakeusers${i}`,
-    email:`fakeuser${i}@anythinks.com`,
-    bio: 'test bio',
-    image: 'https://picsum.photos/200',
-    role: 'user',
-    favorites: [],
-    following: [],
- }))
-
- for(let user of users){
-    const u = new User(user)
-
-    const dbItem = await u.save()
-
-    if(!userId){
-        userId = dbItem._id;
-
+        if (!existingUser) {
+            const u = new User(user);
+            await u.save();
+        }
     }
- }
 
- const items = Array.from(Array(100)).map((_item,i)=>({
-    slug:`fakeitems${i}`,
-    title: `Fake Item${i}`,
-    description: 'test description',
-    image: 'https://picsum.photos/200',
-    comments: [],
-    tagList: ['test','tag'],
-    seller: userId 
- }))
+    const createdUsers = await User.find();
 
- for(item of items){
-    const it = new Item(item)
+    const items = Array.from(Array(100)).map((_item, i) => ({
+        slug: `fakeitem${i}`,
+        title: `Fake Item${i}`,
+        description: 'test description',
+        image: 'https://picsum.photos/200',
+        comments: [],
+        tagList: ['test', 'tag'],
+        seller: createdUsers[i]._id,
+    }));
 
-    const dbItem = await it.save();
-
-    if(!itemId){
-        itemId = dbItem._id
+    for (let item of items) {
+        const it = new Item(item);
+        await it.save();
     }
- }
 
+    const createdItems = await Item.find();
 
+    for (let i = 0; i < createdItems.length; i++) {
+        const comments = Array.from(Array(100)).map((_item, j) => ({
+            body: `This is the body ${j}`,
+            seller: createdUsers[j]._id,
+            item: createdItems[i]._id,
+        }));
 
- const comments = Array.from(Array(100)).map((_item,i)=>({
-    body: 'This is the body',
-    seller: userId,
-    item: itemId,
-
- }))
-
- for(comment of comments){
-
-    const c = new Comment(comment);
-
-    await c.save();
- }
-
-
+        for (let comment of comments) {
+            const c = new Comment(comment);
+            await c.save();
+        }
+    }
 }
 
-
-
-seedDatabase().then(() =>{
-    process.exit();
-}).catch((err)=>{
-    console.error(err);
-    process.exit()
-})
+seedDatabase()
+    .then(() => {
+        console.log('Database seeded successfully.');
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.error('Error seeding database:', err);
+        process.exit(1);
+    });

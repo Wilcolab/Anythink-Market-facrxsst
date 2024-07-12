3aee7b8f62d3f08a0de77ef7351fa1033a659c28
Hitesh Chouhan <24716760+hiteshchouhan22@users.noreply.github.com>
2024-07-12 09:48:32 +0000

    added POST methord in items route

--- backend/main.go
+++ backend/main.go
@@ -10,12 +10,16 @@ type Item struct {
     Name string `json:"name"`
 }
 
+var inventory []Item
+var lastID int = 0
+
 func main() {
 	router := gin.Default()
 	router.GET("/", greet)
 	router.HEAD("/healthcheck", healthcheck)
 
-	router.GET("/items",jsonItems)
+	router.GET("/items",getItems)
+	router.POST("/items", addItem)
 
 	router.Run()
 }
@@ -24,18 +28,35 @@ func greet(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, "Welcome, Go navigator, to the Anythink cosmic catalog.")
 }
 
-func jsonItems(c *gin.Context) {
-	items := []Item{
-		{ID: 1, Name: "Galactic Goggles"},
-		{ID: 2, Name: "Meteor Muffins"},
-		{ID: 3, Name: "Alien Antenna Kit"},
-		{ID: 4, Name: "Starlight Lantern"},
-		{ID: 5, Name: "Quantum Quill"},
-	}
+func getItems(c *gin.Context) {
+	// items := []Item{
+	// 	{ID: 1, Name: "Galactic Goggles"},
+	// 	{ID: 2, Name: "Meteor Muffins"},
+	// 	{ID: 3, Name: "Alien Antenna Kit"},
+	// 	{ID: 4, Name: "Starlight Lantern"},
+	// 	{ID: 5, Name: "Quantum Quill"},
+	// }
 
-	c.JSON(http.StatusOK, items)
+	c.JSON(http.StatusOK, inventory)
 }
 
+func addItem(c *gin.Context){
+	var newItem Item
+
+	if err := c.BindJSON(&newItem); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+
+    // Assign a new ID and add to inventory
+    lastID++
+    newItem.ID = lastID
+    inventory = append(inventory, newItem)
+
+    // Return the newly added item
+    c.JSON(http.StatusCreated, newItem)
+}
 
 func healthcheck(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
